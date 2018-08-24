099ff7e895976f82dc773ae195fd417d8f231039
Leah Joyce <leahpjoyce@gmail.com>
2018-08-24 01:29:48 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div onClick = {this.props.onClick}>This div has been clicked {this.props.clicks} times.</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+let model = { clicks: 0 };
+
+function render() {
+ReactDOM.render(<App
+  clicks={model.clicks}
+  onClick={()=> {model.clicks += 1; render(); }}/>, document.getElementById('root'));
+
+}
+
+render();
+registerServiceWorker();
